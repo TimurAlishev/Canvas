7743ea837854d06657e248c4ac1efcd3df3c5343
tomas <eylertomas@gmail.com>
2020-08-17 22:25:29 +0500

    commit

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-
+from kivy.uix.button import Button
+from random import random
+from kivy.core.window import Window
 from kivy.graphics import (Color, Ellipse, Rectangle, Line)
 
 class PainterWidget(Widget):
-    def __init__(self, **kwargs):
-        super(PainterWidget, self).__init__(**kwargs)
-
+    def on_touch_down(self, touch):
         with self.canvas:
-            Color(0,1,0,1)
-            Rectangle(pos=(100,100), size=(50,50))
+            Color(random(), random(), random(), 1)
+            rad = 50
+            Ellipse(pos = (touch.x - rad/2, touch.y - rad/2), size = (rad, rad))
+            touch.ud['line'] = Line (points = (touch.x, touch.y), width = 25)
+
+
+    def on_touch_move(self, touch):
+        touch.ud['line'].points += (touch.x, touch.y)
 
 class PaitApp(App):
     def build(self):
-        return PainterWidget()
+        parent = Widget()
+        self.painter = PainterWidget()
+        parent.add_widget(self.painter)
+        parent.add_widget(Button(text = 'Clear', on_press = self.clear_canvas))
+        parent.add_widget(Button(text='Save', on_press=self.save, pos = (100, 0)))
+        return parent
+
+    def clear_canvas(self, instance):
+        self.painter.canvas.clear()
+
+    def save(self, instance):
+        self.painter.size = (Window.size[0], Window.size[1])
+        self.painter.export_to_png('image.png')
 
 if __name__ == '__main__':
     PaitApp().run()
